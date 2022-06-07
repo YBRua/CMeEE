d21470f6932c39916d051d84837a9bbd2210d4f8
ybirua <ybirua@sjtu.edu.cn>
2022-06-07 17:11:40 +0800

    Updated run_cmeee to support running GlobalPtr

--- src/run_cmeee.py
+++ src/run_cmeee.py
@@ -1,6 +1,3 @@
-
-import json
-from os.path import join
 from typing import List
 
 from transformers import set_seed, BertTokenizer, Trainer, HfArgumentParser, TrainingArguments
@@ -28,7 +25,10 @@ from metrics import (
     MetricsForBIOTagging,
     MetricsForNestedBIOTagging,
     MetricsForGlobalPtr,
-    extract_entities_biotagging
+)
+from result_gen import (
+    gen_result_bio_tagging,
+    gen_result_global_ptr
 )
 
 MODEL_CLASS = {
@@ -70,34 +70,7 @@ def get_model_with_tokenizer(model_args):
     return model, tokenizer
 
 
-def generate_testing_results(train_args, logger, predictions, test_dataset, for_nested_ner=False):
-    assert len(predictions) == len(test_dataset.examples), \
-        f"Length mismatch: predictions({len(predictions)}), test examples({len(test_dataset.examples)})"
 
-    if not for_nested_ner:
-        pred_entities1 = extract_entities_biotagging(predictions[:, 1:], for_nested_ner=False)
-        pred_entities2 = [[]] * len(pred_entities1)
-    else:
-        pred_entities1 = extract_entities_biotagging(predictions[:, 1:, 0], for_nested_ner=True, first_labels=True)
-        pred_entities2 = extract_entities_biotagging(predictions[:, 1:, 1], for_nested_ner=True, first_labels=False)
-
-    final_answer = []
-
-    for p1, p2, example in zip(pred_entities1, pred_entities2, test_dataset.examples):
-        text = example.text
-        entities = []
-        for start_idx, end_idx, entity_type in p1 + p2:
-            entities.append({
-                "start_idx": start_idx,
-                "end_idx": end_idx,
-                "type": entity_type,
-                "entity": text[start_idx: end_idx + 1],
-            })
-        final_answer.append({"text": text, "entities": entities})
-
-    with open(join(train_args.output_dir, "CMeEE_test.json"), "w", encoding="utf8") as f:
-        json.dump(final_answer, f, indent=2, ensure_ascii=False)
-        logger.info(f"`CMeEE_test.json` saved")
 
 
 def main(_args: List[str] = None):
@@ -139,14 +112,16 @@ def main(_args: List[str] = None):
     # ===== Trainer =====
     if model_args.head_type == 'global_ptr':
         compute_metrics = MetricsForGlobalPtr()
+        collate_fn = CollateFnForGlobalPtr(tokenizer.pad_token_id)
     else:
         compute_metrics = MetricsForNestedBIOTagging() if for_nested_ner else MetricsForBIOTagging()
+        collate_fn = CollateFnForNER(tokenizer.pad_token_id, for_nested_ner=for_nested_ner)
     
     trainer = Trainer(
         model=model,
         tokenizer=tokenizer,
         args=train_args,
-        data_collator=CollateFnForNER(tokenizer.pad_token_id, for_nested_ner=for_nested_ner),
+        data_collator=collate_fn,
         train_dataset=train_dataset,
         eval_dataset=dev_dataset,
         compute_metrics=compute_metrics,
@@ -159,12 +134,31 @@ def main(_args: List[str] = None):
             logger.info("Keyboard interrupt")
 
     if train_args.do_predict:
-        test_dataset = SeqTagDataset(data_args.cblue_root, "test", data_args.max_length, tokenizer, for_nested_ner=for_nested_ner)
+        if model_args.head_type == 'global_ptr':
+            test_dataset = GlobalPtrDataset(
+                data_args.cblue_root,
+                "test", data_args.max_length,
+                tokenizer, for_nested_ner=for_nested_ner)
+        else:
+            test_dataset = SeqTagDataset(
+                data_args.cblue_root,
+                "test", data_args.max_length,
+                tokenizer, for_nested_ner=for_nested_ner)
         logger.info(f"Testset: {len(test_dataset)} samples")
 
         # np.ndarray, None, None
         predictions, _labels, _metrics = trainer.predict(test_dataset, metric_key_prefix="predict")
-        generate_testing_results(train_args, logger, predictions, test_dataset, for_nested_ner=for_nested_ner)
+
+        if model_args.head_type == 'global_ptr':
+            gen_result_global_ptr(
+                train_args, logger,
+                predictions, test_dataset,
+                for_nested_ner=for_nested_ner)
+        else:
+            gen_result_bio_tagging(
+                train_args, logger,
+                predictions, test_dataset,
+                for_nested_ner=for_nested_ner)
 
 
 if __name__ == '__main__':
